7931b290ffb111488c7ae19ad8cece6f1dfe2f3f
d.amirkhanov <doni-rio@yandex.ru>
2021-08-05 00:49:52 +0300

    test

--- internal/actions/test/index.js
+++ internal/actions/test/index.js
@@ -13,7 +13,11 @@ const repo = payload.repository.name;
   console.log(repo);
   console.log(pullNumber);
   console.log(url);
-  const result = await get(url, {headers: `Authorization: token ${process.env.TOKEN}`});
+  const result = await get(url, {
+    headers: `Authorization: token ${process.env.TOKEN}`,
+    'Accept': 'application/json',
+    'Content-type': 'application/json'
+  });
   console.log(result.body);
   const files = JSON.parse(result.body).map(({filename}) => filename);
   console.log(files);
